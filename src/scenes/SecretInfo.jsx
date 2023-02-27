20e55a1baa3f80d9b89b2378138735e121bb71d4
Gisele Abzalov <gisele.abzalov@gmail.com>
2023-02-27 14:32:27 -0500

    added port

--- src/scenes/SecretInfo.jsx
+++ src/scenes/SecretInfo.jsx
@@ -1,7 +1,21 @@
-export default function SecretInfo () {
+import { useEffect, useState } from "react"
+
+export default function SecretInfo() {
+    const [secretStuff, setSecretStuff] = useState()
+    useEffect( () => {
+        //make a secure call to API to get secret info
+        fetch('http://localhost:3030/secrets')
+        .then(res => res.json())
+        .then(data => setSecretStuff(data.message))
+        .catch(alert)
+    }, [])
     return (
     <>
-    <h1>Secret Info</h1>
+        <h1>Secret Info</h1>
+        {secretStuff
+            ? <h2>{secretStuff}</h2>
+            : <h2>Loading...</h2>
+        }
     </>
     )
 }
